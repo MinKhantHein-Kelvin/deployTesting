ffcb58f5c7638a1a0b5ce0b608d7083f9adb8805
Min Khant Hein <mk.hein@mit.com.mm>
2023-05-07 19:19:12 +0630

    upd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const categoryRouter = require ("./routes/categoryRoute");
-const db = require('./models');
+// const db = require('./models');
 
 
 
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use("/", categoryRouter);
 
 
-db.sequelize.sync().then((req)=>{
+// db.sequelize.sync().then((req)=>{
     app.listen(3000, ()=>{
         console.log("Server is running at port 3001");
     })
-})
+// })
